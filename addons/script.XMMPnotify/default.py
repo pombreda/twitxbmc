507a9c1d97c2f1aee527c926374528db5c5a5ea0
eraser1981@googlemail.com <eraser1981@googlemail.com@3a5ed9de-a4a4-b834-7114-160d66a4ff91>
2010-10-12 10:45:26 +0000

    New addon "XMPP notify" initial

---
+++ addons/script.XMMPnotify/default.py
@@ -0,0 +1,138 @@
+﻿#
+#Copyright (C) 2009  Steven J. Burch
+#
+#This program is free software: you can redistribute it and/or modify
+#it under the terms of the GNU General Public License as published by
+#the Free Software Foundation, either version 3 of the License, or
+#(at your option) any later version.
+#
+#This program is distributed in the hope that it will be useful,
+#but WITHOUT ANY WARRANTY; without even the implied warranty of
+#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#GNU General Public License for more details.
+#
+#You may obtain a copy of this license at:
+#  http://www.gnu.org/licenses/gpl-3.0.html
+
+
+#Standard modules
+import os
+import sys
+#Third-party modules
+import xbmcaddon
+#Project modules
+
+
+
+__settings__ = xbmcaddon.Addon(id='script.XMPPnotify')
+__language__ = __settings__.getLocalizedString
+###Path handling
+rootDir = os.getcwd()
+if rootDir[-1] == ';':rootDir = rootDir[0:-1]
+resDir = os.path.join(rootDir, 'resources')
+libDir = os.path.join(resDir, 'lib')
+skinsDir = os.path.join(resDir, 'skins')
+
+sys.path.append (libDir)
+
+from utilities import *
+
+import xmpp
+    
+bot = None
+AUTO_RESTART=0
+
+Debug('Entering idle state...', False)
+smallicon = xbmc.translatePath( os.path.join( skinsDir, 'Default', 'media', 'smallxmpp.png' ) )
+xbmc.executebuiltin('Notification(XMPP notify,Load addon...,3000,' + smallicon + ')')
+
+######################################################################
+
+def start():
+    global bot
+    global LOGGEDIN
+    LOGGEDIN = 0
+    acc = xmpp.JID(__settings__.getSetting( "Login" )) 
+    user,server=acc.getNode(),acc.getDomain()
+    bot = xmpp.Client(server,debug=[])
+    
+    if bot.connect():
+        print "Connected"
+    else:
+        print "Couldn't connect"
+        sys.exit(1)
+        return
+
+    if bot.auth(user,__settings__.getSetting( "Password" ),__settings__.getSetting( "Resource")):
+        print 'Logged In'
+    else:
+        print "Auth error: eek -> ", bot.lastErr, bot.lastErrCode
+        time.sleep(60) # sleep for 10 seconds
+        sys.exit(1)
+        return
+       
+    bot.roster = bot.getRoster()
+    
+    
+    
+    
+    bot.RegisterDisconnectHandler(bot.reconnectAndReauth)
+    bot.RegisterHandler('message',message_callback)
+    bot.sendInitPresence()
+    print "Bot started."
+
+    presence = xmpp.Presence(status = "", show = "online", priority = __settings__.getSetting( "priority"))
+    bot.send(presence)
+    
+    LOGGEDIN = 1
+
+    while 1:
+        bot.Process(10)
+
+def message_callback(conn,mess):
+    try:
+        msg = mess.__str__()#.encode('utf8')
+        user=mess.getFrom()
+        user=str(user).split('/')
+        user=user[0]
+        text = mess.getBody()
+        #print text
+        if ( text == None ):
+            return
+        smallicon = xbmc.translatePath( os.path.join( skinsDir, 'Default', 'media', 'smallxmpp.png' ) )
+        xbmc.executebuiltin('Notification('+user.encode('utf-8','ignore')+',' + text.encode('utf-8','ignore') + ',5000,' + smallicon + ')')
+
+        
+    except Exception ,x:
+        print "Error-"+ str(x)
+        
+    
+
+if __name__ == "__main__":
+    try:	
+        #config = loadConfig()
+        start()
+    except KeyboardInterrupt:
+        print 'INTERRUPT'
+        
+        sys.exit(1)
+    except:
+        if AUTO_RESTART:
+            if sys.exc_info()[0] is not SystemExit:
+                traceback.print_exc()
+            try:
+                bot.disconnected()
+            except IOError:
+                # IOError is raised by default DisconnectHandler
+                pass
+            try:
+                time.sleep(3)
+            except KeyboardInterrupt:
+                print 'INTERRUPT'
+                sys.exit(1)
+            print 'RESTARTING'
+            os.execl(sys.executable, sys.executable, sys.argv[0])
+        else:
+            raise
+
+#EOF
