ca7cc2ee4b71613c608ab13eaa01c7a8b0081fef
eraser1981@googlemail.com <eraser1981@googlemail.com@3a5ed9de-a4a4-b834-7114-160d66a4ff91>
2010-10-26 05:56:41 +0000

    add: ScreenSaver.Clock v.0.0.1

---
+++ addons/script.Screensaver.Clock/default.py
@@ -0,0 +1,23 @@
+﻿#Standard modules
+import os
+import sys
+#Third-party modules
+import xbmcaddon
+#Project modules
+
+
+__settings__ = xbmcaddon.Addon(id='script.Screensaver.Clock')
+__language__ = __settings__.getLocalizedString
+###Path handling
+rootDir = os.getcwd()
+if rootDir[-1] == ';':rootDir = rootDir[0:-1]
+resDir = os.path.join(rootDir, 'resources')
+libDir = os.path.join(resDir, 'lib')
+skinsDir = os.path.join(resDir, 'skins')
+
+sys.path.append (libDir)
+
+import gui
+ui = gui.GUI("Generic.xml" , os.getcwd(), "Default")
+ui.doModal()
+    
